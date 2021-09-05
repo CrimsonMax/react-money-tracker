fd2aebdddad5cd89ac0d912fefe1292e53b371cf
CrimsonMax <61148415+CrimsonMax@users.noreply.github.com>
2021-09-05 13:32:48 +0300

    MERN part 1

--- client/src/context/AppReducer.js
+++ client/src/context/AppReducer.js
@@ -1,4 +1,4 @@
-export default (state, action) => {
+const AppReducer = (state, action) => {
   switch (action.type) {
     case 'DELETE_TRANSACTION':
       return {
@@ -15,3 +15,5 @@ export default (state, action) => {
       return state
   }
 }
+
+export default AppReducer
